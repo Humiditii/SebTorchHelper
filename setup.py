5f787e99bd0bca6f9ca0be238a888b287ea7cb1a
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-12-22 15:09:45 +0100

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'storch',
+  packages = ['storch'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A pytorch trainner for neural networks',
+  author = 'Babatunde A.O',
+  author_email = 'humiditii45@gmail.com',
+  url = 'https://github.com/Humiditii/SebTorchHelper',
+  download_url = 'https://github.com/Humiditii/SebTorchHelper',
+  keywords = ['add', 'sub', 'tests'], 
+  classifiers = [],
+)
